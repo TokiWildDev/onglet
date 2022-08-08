511bfa3cd06fe55adc7055c15034668c9c73aee6
TokiWildDev <tokiworldwilddev@gmail.com>
2022-08-08 20:58:54 +0200

    mise en place du html css et JVS

--- javascript/onglet.js
+++ javascript/onglet.js
@@ -1,1 +1,34 @@
 console.log('HelloWorld');
+
+const buttons = document.getElementsByClassName('containOnglet__containButton__button')
+const text = document.getElementsByClassName('containOnglet__containText')
+
+const removeText = () =>
+{
+    
+    for(let j=0; j<text.length; j++)
+    {
+        text[j].classList.remove('containOnglet__containText--active')
+    }
+}
+const removeButtons = ()=>
+{
+    for(let j=0; j<buttons.length; j++)
+    {
+        buttons[j].classList.remove('containOnglet__containButton__button--active')
+    }
+}  
+
+
+
+for(let i=0; i<buttons.length; i++)
+{
+    buttons[i].addEventListener('click', ()=>
+    {
+        removeButtons();
+        removeText();
+        text[i].classList.add('containOnglet__containText--active')
+        buttons[i].classList.add('containOnglet__containButton__button--active')          
+   
+    });
+};
